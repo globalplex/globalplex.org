eb64e0353610e7a0a25abe054ff93313686cd757
Jacky Lee <dr.jackylee@gmail.com>
2020-07-31 02:50:13 +0800

    Add functionality for creating pages from markdown files

---
+++ gatsby-node.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const { createPage } = actions;
+
+  const result = await graphql(`
+    {
+      allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+        edges {
+          node {
+            frontmatter {
+              path
+              templateKey
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`);
+    return;
+  }
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.frontmatter.path,
+      component: path.resolve(
+        `src/templates/${String(node.frontmatter.templateKey)}.js`
+      ),
+    });
+  });
+};
