b217ba465eacbceb574c987ce5e48c4dd0a44c98
Jacky Lee <dr.jackylee@gmail.com>
2020-09-18 08:16:49 -0700

    Refactor index page

---
+++ src/components/IndexSection.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../pages/mystyles.scss";
+
+const IndexSection = ({ children, subtitle, title }) => (
+  <section className="section">
+    <div className="container">
+      <h2 className="index-section-title">{title}</h2>
+      <h3 className="py-4 title">{subtitle}</h3>
+      {children}
+    </div>
+  </section>
+);
+
+export default IndexSection;
--- src/templates/index.js
+++ src/templates/index.js
@@ -3,8 +3,10 @@ import { graphql, Link } from "gatsby";
 import Layout from "../components/Layout";
 import TestimonialsCard from "../components/TestimonialsCard";
 import ContentCard from "../components/ContentCard";
+import IndexSection from "../components/IndexSection";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "../utils/fontawesome";
+import "../pages/mystyles.scss";
 
 const IndexPage = ({ data }) => {
   const {
@@ -17,38 +19,33 @@ const IndexPage = ({ data }) => {
 
   return (
     <Layout>
-      <section
-        className="hero is-dark is-bold is-large is-fullheight"
-        style={{
-          backgroundPosition: "center",
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-          backgroundImage: `url(../img/home-cover.jpg)`,
-        }}
-      >
-        <div className="hero-body">
-          <div className="container">
-            <p className="title is-1 has-text-left">{landing.title}</p>
-            <p className="subtitle has-text-left">
-              Our mission is to effectively equip people to excel in life and
-              expand their influence
-            </p>
-            <Link to="/contact/">
-              <button className="button is-outlined is-primary is-medium has-text-weight-semibold">
-                Contact Us
-              </button>
-            </Link>
+      <div className="index-sections">
+        <section
+          className="hero is-dark is-bold is-large is-fullheight"
+          style={{
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundImage: `url(../img/home-cover.jpg)`,
+          }}
+        >
+          <div className="hero-body">
+            <div className="container">
+              <p className="title is-1 has-text-left">{landing.title}</p>
+              <p className="subtitle has-text-left">
+                Our mission is to effectively equip people to excel in life and
+                expand their influence
+              </p>
+              <Link to="/contact/">
+                <button className="button is-outlined is-primary is-medium has-text-weight-semibold">
+                  Contact Us
+                </button>
+              </Link>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
 
-      {/* What we offer section */}
-      <section className="section is-medium">
-        <div className="container">
-          <p className="subtitle">{services.title}</p>
-          <h2 className="title">{services.blurb}</h2>
-          <br />
-          <br />
+        <IndexSection subtitle={services.blurb} title={services.title}>
           <div className="columns is-vcentered">
             <div className="column">
               <figure className="image">
@@ -85,16 +82,9 @@ const IndexPage = ({ data }) => {
               ))}
             </div>
           </div>
-        </div>
-      </section>
+        </IndexSection>
 
-      {/* Our Approach section */}
-      <section className="section is-medium has-background-light">
-        <div className="container">
-          <p className="subtitle">{approach.title}</p>
-          <h2 className="title">{approach.blurb}</h2>
-          <br />
-          <br />
+        <IndexSection subtitle={approach.blurb} title={approach.title}>
           <div className="columns">
             {approach.content.map((item) => (
               <div className="column">
@@ -122,16 +112,9 @@ const IndexPage = ({ data }) => {
               </button>
             </Link>
           </div>
-        </div>
-      </section>
+        </IndexSection>
 
-      {/* Customer Stories section */}
-      <section className="section is-medium">
-        <div className="container">
-          <p className="subtitle">{testimonials.title}</p>
-          <h2 className="title">{testimonials.blurb}</h2>
-          <br />
-          <br />
+        <IndexSection subtitle={testimonials.blurb} title={testimonials.title}>
           <div className="columns">
             {testimonials.content.map((item) => (
               <div className="column">
@@ -161,29 +144,28 @@ const IndexPage = ({ data }) => {
               </button>
             </Link>
           </div>
-        </div>
-      </section>
+        </IndexSection>
 
-      <section className="section has-background-light">
-        <div className="container">
-          <div className="box has-background-dark">
-            <div className="section columns has-text-white is-vcentered is-medium">
-              <div className="column is-1" />
-              <div className="column is-6">
-                <h1 className="title is-1 has-text-white">{consult.title}</h1>
-                <p className="has-text-white">{consult.blurb}</p>
-              </div>
-              <div className="column has-text-centered">
-                <Link to={consult.callToActionLink}>
-                  <button className="button is-outlined is-primary is-medium has-text-weight-semibold">
-                    Contact Us
-                  </button>
-                </Link>
+        <section className="section">
+          <div className="container">
+            <div className="box has-background-dark">
+              <div className="columns is-vcentered section">
+                <div className="column px-6">
+                  <p className="has-text-white title">{consult.title}</p>
+                  <p className="has-text-light">{consult.blurb}</p>
+                </div>
+                <div className="column is-narrow has-text-centered px-6">
+                  <Link to={consult.callToActionLink}>
+                    <button className="button is-outlined is-primary is-medium has-text-weight-semibold">
+                      Contact Us
+                    </button>
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </Layout>
   );
 };
